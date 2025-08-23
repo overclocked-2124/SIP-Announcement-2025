43e0bbade1dd2acd261e56d17d303a23714eeb62
Sumukha_Upadhyaya <146051934+overclocked-2124@users.noreply.github.com>
2025-08-23 16:48:53 +0530

    changed date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ const announcements = [
   },
   {
     title: "Club Showcasing Day Brochure",
-    date: "22 Aug 2025",
+    date: "23 Aug 2025",
     driveShare: "https://drive.google.com/file/d/106w2fXB3x3961LfGBTvww39BsEGuMx6P/view?usp=sharing",
     type: "clubs"
   },
