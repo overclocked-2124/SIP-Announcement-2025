6745f3de754fac5e02717589a920d229c357dfdc
Sumukha Upadhyaya <146051934+overclocked-2124@users.noreply.github.com>
2025-09-17 20:31:36 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -159,8 +159,8 @@ const AnnouncementCard = ({ announcement }: { announcement: typeof announcements
 
 const notifications: NotificationItem[] = [
 {
-    message: "Press on this to access timetable and section list for C and P cycle students updated as of 15 Sept 2025.",
-    timestamp: "15 Sept 2025",
+    message: "Press on this to access timetable and section list for C and P cycle students updated as of 17 Sept 2025.",
+    timestamp: "17 Sept 2025",
     link: "https://drive.google.com/drive/folders/1dncMKlqPBAVWpk2fZMVNgtBoFQrYU3cm",
   },
 {
